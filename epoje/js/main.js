4987ef3f3e8b24dfa635ff8695e1db5ae36e8f7e
CristianXDev <cristianxdev@gmail.com>
2026-03-18 08:56:25 -0400

    Add files via upload

---
+++ epoje/js/main.js
@@ -0,0 +1,9 @@
+// Lógica para cambiar la navbar al hacer scroll
+window.addEventListener('scroll', function() {
+    const nav = document.getElementById('mainNav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+}); 
